8f5c195bb590c3ddee3b59c2a1dcb53f5e1a9501
Sasun Madhuranga <gsimadhuranga@gmail.com>
2026-05-03 14:57:12 +0530

    Updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true
-};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://interview-alb-1315898996.us-east-1.elb.amazonaws.com/api/:path*',
+      },
+    ]
+  },
+}
 
 export default nextConfig;
